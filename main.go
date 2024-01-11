b808c4e6ce8cad44460aede39cdbd6fb44389ca2
Amit Patel <amit97911@gmail.com>
2024-01-11 19:27:54 +0530

    Mux server works

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := mux.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// an example API handler
+		json.NewEncoder(w).Encode(map[string]string{"response": "Hello World!"})
+	})
+	defer log.Fatal(srv.ListenAndServe())
 }
